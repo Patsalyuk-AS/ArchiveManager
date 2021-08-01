ee2eddc00b9a2a2e2466d46b6152cb8bba0e930f
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-08-01 23:47:43 +0300

    Поправил именование

--- src/main/java/com/github/patsalyukas/archivemanager/controllers/DocumentController.java
+++ src/main/java/com/github/patsalyukas/archivemanager/controllers/DocumentController.java
@@ -31,13 +31,13 @@ public class DocumentController {
         return new ResponseEntity<>(documentService.update(id, document), HttpStatus.OK);
     }
 
-    @GetMapping("/box/{boxCode}")
+    @GetMapping("/box/{boxId}")
     public ResponseEntity<List<Document>> getDocumentsInBox(@PathVariable Long boxId) {
         List<Document> documents = documentService.getDocumentsInBox(boxId);
         return (documents != null) ? new ResponseEntity<>(documents, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/box/{id}")
+    @PutMapping("/box/{boxId}")
     public ResponseEntity<Document> putDocumentInBox(@PathVariable Long boxId, @RequestBody Document document) {
         return new ResponseEntity<>(documentService.putDocumentInBox(boxId, document), HttpStatus.OK);
     }
