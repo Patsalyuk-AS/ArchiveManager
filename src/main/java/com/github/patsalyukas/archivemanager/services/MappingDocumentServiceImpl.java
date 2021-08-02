fed8006627e65580db55555ca01172fa7608f922
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-08-02 23:42:59 +0300

    Добавил класс DocumentMappingImpl.

---
+++ src/main/java/com/github/patsalyukas/archivemanager/services/MappingDocumentServiceImpl.java
@@ -0,0 +1,27 @@
+package com.github.patsalyukas.archivemanager.services;
+
+import com.github.patsalyukas.archivemanager.dto.DocumentDTO;
+import com.github.patsalyukas.archivemanager.entities.Document;
+
+public class MappingDocumentServiceImpl implements MappingDocumentService {
+
+    MappingBoxService mappingBoxService;
+
+    @Override
+    public Document mapToDocumentEntity(DocumentDTO documentDTO) {
+        Document document = new Document(documentDTO.getName(), documentDTO.getCode());
+        if (documentDTO.getBoxDTO() != null) {
+            document.setBox(mappingBoxService.mapToBoxEntity(documentDTO.getBoxDTO()));
+        }
+        return document;
+    }
+
+    @Override
+    public DocumentDTO mapToDocumentDTO(Document document) {
+        DocumentDTO documentDTO = new DocumentDTO(document.getName(), document.getCode(), null);
+        if (document.getBox() != null) {
+            documentDTO.setBoxDTO(mappingBoxService.mapToBoxDTO(document.getBox()));
+        }
+        return documentDTO;
+    }
+}
