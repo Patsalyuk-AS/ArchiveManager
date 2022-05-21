48ebf82e9730aef86a12228184cdd76f3d44837d
Andrey Patsalyuk <pazalyuk@gmail.com>
2022-05-21 22:55:01 +0300

    ДОбавлен Swagger

--- src/main/java/com/github/patsalyukas/archivemanager/controllers/BoxController.java
+++ src/main/java/com/github/patsalyukas/archivemanager/controllers/BoxController.java
@@ -6,6 +6,7 @@ import com.github.patsalyukas.archivemanager.entities.Box;
 import com.github.patsalyukas.archivemanager.services.BoxService;
 import com.github.patsalyukas.archivemanager.services.MappingBoxService;
 import com.github.patsalyukas.archivemanager.services.MappingDocumentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,7 @@ public class BoxController {
     MappingDocumentService mappingDocumentService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Find box by ID")
     public BoxDTO findBoxById(@PathVariable Long id) {
         Box box = boxService.findBoxById(id);
         return (mappingBoxService.mapToBoxDTO(box));
@@ -30,18 +32,21 @@ public class BoxController {
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create box")
     public BoxDTO create(@RequestBody BoxDTO boxDTO) {
         Box box = mappingBoxService.mapToBoxEntity(boxDTO);
         return mappingBoxService.mapToBoxDTO(boxService.create(box));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update box")
     public BoxDTO update(@PathVariable Long id, @RequestBody BoxDTO boxDTO) {
         Box box = boxService.update(id, mappingBoxService.mapToBoxEntity(boxDTO));
         return mappingBoxService.mapToBoxDTO(box);
     }
 
     @GetMapping("/documents/{boxId}")
+    @Operation(summary = "Get documents in a box by box ID")
     public Set<DocumentDTO> getDocumentsInBox(@PathVariable Long boxId) {
         return boxService.getDocumentsInBox(boxId).stream().map(mappingDocumentService::mapToDocumentDTO).collect(Collectors.toSet());
     }
